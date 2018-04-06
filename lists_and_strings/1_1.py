c34807566446fc6fd67865b9d46842c293efaecb
eden1991 <mail@edenhalil.com>
2018-04-06 16:21:19 +1000

    Refactored solutions 1.1 and 1.2 and added tests

--- lists_and_strings/1_1.py
+++ lists_and_strings/1_1.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python3
+import unittest
 
 # Task description
 # Implement an algorithm to determine if a string has all unique characters. What if you
 # can not use additional data structures?
 
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_checker(self):
+        self.assertFalse(check_unique("Pool"))
+        self.assertTrue(check_unique("Police"))
+        self.assertTrue(check_unique("Endeavor"))
+        self.assertTrue(check_unique("AWS"))
+        self.assertFalse(check_unique("Google"))
+        self.assertFalse(check_unique("ghwuaighiaewhgjkwea"))
+
+
 def check_unique(word):
     buffer = ""
+    unique = True
     # Iterate through the string passed in to the function
     for letter in word:
         # Check if the letter exists in the buffer string.
         # If it exists, then break out from the loop as the string is not unique.
         if letter in buffer:
-            print("The string does not contain all unique characters.")
+            unique = False
             break
         else:
             # We've encountered the first occurrence of letter, so append it in to
             # the buffer for comparison in the next iteration of the loop
             buffer = buffer + letter
 
-    if buffer == word:
-        print("The string contains all unique letters.")
+    return unique
 
 
 if __name__ == "__main__":
-    check_unique("Pool")
-    check_unique("Police")
-    check_unique("Endeavor")
-    check_unique("AWS")
-    check_unique("Google")
-    check_unique("ghwuaighiaewhgjkwea")
+    unittest.main()
--- lists_and_strings/1_2.py
+++ lists_and_strings/1_2.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
+import unittest
 
 # Task description
 # Write code to reverse a C-Style String. (C-String means that “abcd” is represented as
 # five characters, including the null character.)
 
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_checker(self):
+        self.assertEqual(reverse('Pool\0'), 'looP\0')
+        self.assertEqual(reverse('Police\0'), 'eciloP\0')
+        self.assertEqual(reverse('Monitor\0'), 'rotinoM\0')
+
+
 def reverse(word):
     reversed = ""
     # Loop through each letter of the string passed in to the function from back-to-front
     # and append each letter in to the above variable, excluding the null string at the end.
-    for letter in range(len(word)-1, -1, -1):
+    for letter in range(len(word)-2, -1, -1):
         reversed = reversed + word[letter]
     # Now append the null string
     reversed = reversed + word[len(word)-1]
 
-    print(reversed)
+    return reversed
 
 
 if __name__ == '__main__':
-    reverse('Pool\0')
-    reverse('Police\0')
-    reverse('Monitor\0')
+    unittest.main()
