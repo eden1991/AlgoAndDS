9e1a7a80f6b8cdf4c59911ad7d95cd8d55ccde46
Eden Halil <mail@edenhalil.com>
2018-05-17 13:53:26 +1000

    Add solution 3.1

---
+++ StacksAndQueues/main.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+from stack import Stack
+import unittest
+
+
+class TestStackMethods(unittest.TestCase):
+
+    '''
+    Test general stack functionality
+    '''
+    def test_stack(self):
+        myStack = Stack()
+        numbers = [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+        # Push all the numbers in the numbers list in to myStack
+        for number in numbers:
+            myStack.push(number)
+
+        # The stack shouldn't be empty
+        self.assertFalse(myStack.isEmpty())
+
+        # The element at the head of the stack should be 8
+        self.assertEqual(myStack.peek(), 8)
+
+        # Pop 5 elements out
+        for number in range(0, 5):
+            myStack.pop()
+
+        # The element at the head of the stack should be 3
+        self.assertEqual(myStack.peek(), 3)
+
+        # Empty the stack
+        for number in range(0, myStack.size()):
+            myStack.pop()
+
+        # The stack should now be empty
+        self.assertTrue(myStack.isEmpty())
+
+    ''' 
+    3.1 Describe how you could use a single array to implement three stacks.
+
+    Approach: Have an array of size 3 and nest another array at each indice.
+    You can then operate these nested arrays 
+    '''
+    def test_singleArray_three_stacks(self):
+        wardrobe = list()
+        shoes = list()
+        shirts = list()
+        bags = list()
+
+        wardrobe.append(shoes)
+        wardrobe.append(shirts)
+        wardrobe.append(bags)
+
+        # There should be 3 empty lists nested inside of the wardrobe list
+        self.assertEqual(wardrobe, [[], [], []])
+
+        # Add some items in for verifying
+        wardrobe[0].append("Nike")
+        wardrobe[0].append("Adidas")
+        wardrobe[1].append("Ralph Polo")
+        wardrobe[1].append("Lacoste")
+        wardrobe[1].append("Tom Ford")
+        wardrobe[2].append("Country Road")
+
+        # Now check that those items exist
+        self.assertEqual(wardrobe[0][0], "Nike")
+        self.assertEqual(wardrobe[1][0], "Ralph Polo")
+        self.assertEqual(wardrobe[2][0], "Country Road")
+
+        # Let's see that we can remove items from the head of each stack
+        poppedShoe = wardrobe[0].pop()
+
+        # The item that we just popped should be Adidas
+        self.assertEqual(poppedShoe, "Adidas")
+        # The item at the head of the stack should be Nike
+        self.assertEqual(wardrobe[0][0], "Nike")
+        # Check that the shoes stack is empty after popping the last item
+        wardrobe[0].pop()
+        self.assertEqual(len(wardrobe[0]), 0)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ StacksAndQueues/stack.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from node import Node
+
+
+class Stack:
+
+    def __init__(self):
+        self.top = None
+
+    def isEmpty(self):
+        return self.top == None
+
+    def push(self, item):
+        temp = Node(item)
+        temp.setNext(self.top)
+        self.top = temp
+
+    def pop(self):
+        if self.top is not None:
+            item = self.top.getData()
+            self.top = self.top.getNext()
+            return item
+
+        return None
+
+    def peek(self):
+        return self.top.getData()
+
+    def size(self):
+        current = self.top
+        count = 0
+        while current is not None:
+            count += 1
+            current = current.getNext()
+
+        return count
+
+
